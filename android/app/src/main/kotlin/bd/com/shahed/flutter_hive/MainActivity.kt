7c298f124ccacee92134adf825d4dd01965e631a
Shahed <shahed_hridoy@outlook.com>
2023-05-06 16:46:31 +0600

    initial commits

---
+++ android/app/src/main/kotlin/bd/com/shahed/flutter_hive/MainActivity.kt
@@ -0,0 +1,6 @@
+package bd.com.shahed.flutter_hive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  await Hive.openBox('shopping_box');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Hive',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+
+  List<Map<String, dynamic>> _items = [];
+
+  final _shoppingBox = Hive.box('shopping_box');
+
+  void _refreshItems() {
+    final data = _shoppingBox.keys.map((key) {
+      final item = _shoppingBox.get(key);
+      return {
+        "key": key,
+        "name": item["name"],
+        "quantity": item["quantity"],
+      };
+    }).toList();
+
+    setState(() {
+      _items = data.reversed.toList();
+    });
+  }
+
+  Future<void> _createItem(Map<String, dynamic> newItem) async {
+    await _shoppingBox.add(newItem);
+    _refreshItems();
+  }
+
+  void _showForm(BuildContext context, int? itemKey) async {
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        isScrollControlled: true,
+        builder: (_) => Container(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                  top: 15,
+                  left: 15,
+                  right: 15),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(hintText: 'Name'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    controller: _quantityController,
+                    decoration: const InputDecoration(hintText: 'Quantity'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        _createItem({
+                          "name": _nameController.text,
+                          "quantity": _quantityController.text,
+                        });
+
+                        _nameController.text = '';
+                        _quantityController.text = '';
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Create New')),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                ],
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hive'),
+      ),
+      body: ListView.builder(
+          itemCount: _items.length,
+          itemBuilder: (_, index) {
+            final currentItem = _items[index];
+            return Card(
+              color: Colors.orange.shade100,
+              margin: const EdgeInsets.all(10.0),
+              elevation: 3,
+              child: ListTile(
+                title: Text(currentItem['name']),
+                subtitle: Text(currentItem['quantity']),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showForm(context, null),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
