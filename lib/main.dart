ff7570f5af4624aac167c67f95dc7b98a110d523
Shahed <shahed_hridoy@outlook.com>
2023-09-04 16:04:10 +0600

    separating screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hive/home_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Hive.initFlutter();
   await Hive.openBox('shopping_box');
-
   runApp(const MyApp());
 }
 
@@ -20,165 +19,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _quantityController = TextEditingController();
-
-  List<Map<String, dynamic>> _items = [];
-
-  final _shoppingBox = Hive.box('shopping_box');
-
-  void _refreshItems() {
-    final data = _shoppingBox.keys.map((key) {
-      final item = _shoppingBox.get(key);
-      return {
-        "key": key,
-        "name": item["name"],
-        "quantity": item["quantity"],
-      };
-    }).toList();
-
-    setState(() {
-      _items = data.reversed.toList();
-    });
-  }
-
-  Future<void> _createItem(Map<String, dynamic> newItem) async {
-    await _shoppingBox.add(newItem);
-    _refreshItems();
-  }
-
-  Future<void> _updateItem(int itemKey, Map<String, dynamic> item) async {
-    await _shoppingBox.put(itemKey, item);
-    _refreshItems();
-  }
-
-  Future<void> _deleteItem(int itemKey) async {
-    await _shoppingBox.delete(itemKey);
-    _refreshItems();
-  }
-
-  void _showForm(BuildContext context, int? itemKey) async {
-    if (itemKey != null) {
-      final existingItem =
-          _items.firstWhere((element) => element["key"] == itemKey);
-      _nameController.text = existingItem["name"];
-      _quantityController.text = existingItem["quantity"];
-    }
-
-    showModalBottomSheet(
-        context: context,
-        elevation: 5,
-        isScrollControlled: true,
-        builder: (_) => Container(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom,
-                  top: 15,
-                  left: 15,
-                  right: 15),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(hintText: 'Name'),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    controller: _quantityController,
-                    decoration: const InputDecoration(hintText: 'Quantity'),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      onPressed: () async {
-                        if (itemKey == null) {
-                          _createItem({
-                            "name": _nameController.text,
-                            "quantity": _quantityController.text
-                          });
-                        }
-
-                        if (itemKey != null) {
-                          _updateItem(itemKey, {
-                            "name": _nameController.text,
-                            "quantity": _quantityController.text
-                          });
-                        }
-
-                        _nameController.text = '';
-                        _quantityController.text = '';
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(itemKey == null ? 'Create New' : 'Update')),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                ],
-              ),
-            ));
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _refreshItems();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hive'),
-      ),
-      body: ListView.builder(
-          itemCount: _items.length,
-          itemBuilder: (_, index) {
-            final currentItem = _items[index];
-            return Card(
-              color: Colors.orange.shade100,
-              margin: const EdgeInsets.all(10.0),
-              elevation: 3,
-              child: ListTile(
-                title: Text(currentItem['name']),
-                subtitle: Text(currentItem['quantity']),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                        onPressed: () => _showForm(context, currentItem['key']),
-                        icon: const Icon(Icons.edit)),
-                    IconButton(
-                        onPressed: () {
-                          _deleteItem(currentItem["key"]);
-                        },
-                        icon: const Icon(Icons.delete)),
-                  ],
-                ),
-              ),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _showForm(context, null),
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
     );
   }
 }
